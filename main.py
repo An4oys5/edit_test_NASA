a1c9623abbad9428fb2f213833a1ccccceae5295
An4oys5 <dmitrievannn16@gmail.com>
2025-05-19 16:16:52 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,35 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+
+# === Настройка браузера в headless-режиме ===
+options = Options()
+options.add_argument("--headless")            
+options.add_argument("--no-sandbox")
+options.add_argument("--disable-dev-shm-usage")
+
+# === Инициализация драйвера ===
+driver = webdriver.Chrome()
+
+try:
+    # 1) Открываем сайт
+    driver.get("https://eyes.nasa.gov/apps/solar-system/")
+    time.sleep(5)  
+
+    # 2) Проверяем заголовок страницы
+    assert "Solar System" in driver.title
+
+    # 3) Находим кнопку меню по ID и делаем скриншот
+    
+    driver.save_screenshot("1_homepage.png")
+    
+    time.sleep(1)
+   
+
+    print("Тест пройден успешно.")
+except Exception as e:
+    print("Ошибка в тесте:", e)
+    driver.save_screenshot("error.png")
+finally:
+    driver.quit()
