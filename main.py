065b2e0906ec5e331bb96f12c6196609ab8ce9a7
An4oys5 <dmitrievannn16@gmail.com>
2025-05-19 16:33:36 +0300

    first test 6

--- main.py
+++ main.py
@@ -3,13 +3,13 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 
-# === Настройка браузера в headless-режиме ===
+# Настройка браузера в headless-режиме 
 options = Options()
 options.add_argument("--headless")            
 options.add_argument("--no-sandbox")
 options.add_argument("--disable-dev-shm-usage")
 
-# === Инициализация драйвера ===
+# Инициализация драйвера 
 driver = webdriver.Chrome()
 
 try:
@@ -20,13 +20,11 @@ try:
     # 2) Проверяем заголовок страницы
     assert "Solar System" in driver.title
 
-    # 3) Находим кнопку меню по ID и делаем скриншот
-    
+    # 3) Делаем скриншот главной страницы
     driver.save_screenshot("1_homepage.png")
     
     time.sleep(1)
    
-
     print("Тест пройден успешно.")
 except Exception as e:
     print("Ошибка в тесте:", e)
